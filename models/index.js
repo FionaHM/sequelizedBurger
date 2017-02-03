43214d8b2a3e266fad5c3faf2613d4b120e51bba
FH <FH@Fionas-MacBook-Air.local>
2017-02-03 13:32:59 -0600

    heroku db config

--- models/index.js
+++ models/index.js
@@ -4,20 +4,10 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.NODE_ENV || 'production';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-  connection =mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'burgers_db'
-  });
-}
 
 
 if (config.use_env_variable) {
